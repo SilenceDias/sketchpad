0efa5144aabd74b0130ee1d0b6ad6b9122d1a840
SilenceDias <11dias02@mail.ru>
2023-05-22 23:07:05 +0600

    Added basic functionality and UI, now user can only scetch with one color

---
+++ script.js
@@ -0,0 +1,46 @@
+const container = document.querySelector(".gridContainer");
+
+function changeColor(div){
+    div.style.backgroundColor = 'red';
+}
+function createPad(size){
+    for(let i = 0; i < size; i++){
+        const row = document.createElement("div");
+        row.classList.add('row');
+        for(let j = 0; j < size; j++){
+            const box = document.createElement("div");
+            box.classList.add('box'); 
+            box.style.height = `${500/size}px`; 
+            box.style.width = `${500/size}px`; 
+            row.appendChild(box);
+        }
+        container.appendChild(row);
+    }
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach(box => {
+        box.addEventListener("mouseover", function(){
+            changeColor(box);
+        })
+    })
+}
+
+createPad(16);
+
+
+
+function changePadSize(){
+    let userChoice = prompt("Enter the size(Max is 100, Min is 2)");
+    if(userChoice>100||userChoice<2){
+        alert("R u idiot? Try again");
+        return;
+    }
+    removePad();
+    createPad(userChoice);
+}
+
+function removePad(){
+    while(container.lastElementChild){
+        container.removeChild(container.lastElementChild);
+    }
+}
